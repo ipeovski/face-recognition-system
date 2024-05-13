b56ba5d9873e7e8995049e31228e64e6e664a9ed
ipeovski <ipeovski19@gmail.com>
2024-05-13 18:18:23 +0300

    upload files

--- app.js
+++ app.js
@@ -23,21 +23,20 @@
     const displaySize = { width: video.width, height: video.height }
     faceapi.matchDimensions(canvas, displaySize);
     setInterval(async () => {
-      const labeledFaceDescriptors = await loadLabeledImages();
-      const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, .6);
+     // const labeledFaceDescriptors = await loadLabeledImages();
+      //const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, .6);
       const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
       const resizedDetections = faceapi.resizeResults(detections, displaySize);
-      const results = resizedDetections.map(d => faceMatcher.findBestMatch(d.descriptor));
       // const LabeledFaceDescriptors = await loadLabeledImages();
       // const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, .6);
 
       canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
     
-      results.forEach((result, i) => {
-        const box = resizedDetections[i].detection.box;
-        const drawBox = new faceapi.draw.DrawBox(box, { label: result.toString() });
-        drawBox.draw(canvas);
-      });
+      // results.forEach((result, i) => {
+      //   const box = resizedDetections[i].detection.box;
+      //   const drawBox = new faceapi.draw.DrawBox(box, { label: result.toString() });
+      //   drawBox.draw(canvas);
+      // });
 
       for (const detection of detections) {
         const box = detection.detection.box;
@@ -47,20 +46,20 @@
     }, 100);
   });
 
-  function loadLabeledImages(){
-    const labels = ['Ivan Peovski', 'Nina Dobrev'];
-    return Promise.all(
-      labels.map(async label => {
-        const descriptions = [];
-        for(let i = 1; i <= 2; i++){
-          const img = await faceapi.fetchImage
-          (`https://github.com/ipeovski/face-recognition-system/tree/161dd8beb06217fde36b701ea2a0440d6b7cfab5/labeled_images/${label}/${i}.jpg`);
-          const detections = await faceapi.detectSingleFace(img)
-          .withFaceLandmarks().withFaceDescriptor();
-          descriptions.push(detections.descriptor);
-        }
+  // function loadLabeledImages(){
+  //   const labels = ['Ivan Peovski', 'Nina Dobrev'];
+  //   return Promise.all(
+  //     labels.map(async label => {
+  //       const descriptions = [];
+  //       for(let i = 1; i <= 2; i++){
+  //         const img = await faceapi.fetchImage
+  //         (`https://github.com/ipeovski/face-recognition-system/tree/161dd8beb06217fde36b701ea2a0440d6b7cfab5/labeled_images/${label}/${i}.jpg`);
+  //         const detections = await faceapi.detectSingleFace(img)
+  //         .withFaceLandmarks().withFaceDescriptor();
+  //         descriptions.push(detections.descriptor);
+  //       }
 
-        return new faceapi.LabeledFaceDescriptors(label, descriptions);
-      })
-    );
-  }
+  //       return new faceapi.LabeledFaceDescriptors(label, descriptions);
+  //     })
+  //   );
+  // }
